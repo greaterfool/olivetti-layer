c8680d595d87c6065e86bc897334c0209964a245
marmalade <summit@tutanota.de>
2017-05-05 11:26:32 -0700

    Removed comment fluff.

--- packages.el
+++ packages.el
@@ -9,24 +9,6 @@
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `olivetti-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `olivetti/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `olivetti/pre-init-PACKAGE' and/or
-;;   `olivetti/post-init-PACKAGE' to customize the package as it is loaded.
-
 ;;; Code:
 
 (defconst olivetti-packages
